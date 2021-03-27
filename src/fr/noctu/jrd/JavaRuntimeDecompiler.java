ed39950566c54cefd3bc888db9591ace172a2391
NOcturnais <NOcturnais1412@gmail.com>
2021-03-27 15:20:39 +0100

    better test

--- src/fr/noctu/jrd/JavaRuntimeDecompiler.java
+++ src/fr/noctu/jrd/JavaRuntimeDecompiler.java
@@ -1,52 +1,9 @@
 package fr.noctu.jrd;
 
-import fr.noctu.jrd.javaobjects.JavaField;
 import fr.noctu.jrd.javaobjects.JavaKlass;
-import fr.noctu.jrd.javaobjects.JavaMethod;
-import fr.noctu.jrd.javaobjects.utils.JavaInstruction;
 
 public class JavaRuntimeDecompiler {
-    public int diplomatic = 2;
-
-    public float test1(int rien){
-        Object[] bruhArray = new Object[1];
-        double oof = 4.2D * 2D;
-        float issou = 21F * diplomatic;
-        Object ya = new Object();
-        if(ya instanceof Thread){
-            System.out.println("oof");
-        }
-        test2();
-        return 2F;
-    }
-
-    private static void test2(){
-        String oui = "oof";
-        System.err.println(oui);
-    }
-
     public JavaKlass decompileClass(Class<?> clazz){
         return new JavaKlass(clazz);
-    }
-
-    // TEST //
-    public static void main(String[] args) {
-        JavaRuntimeDecompiler javaRuntimeDecompiler = new JavaRuntimeDecompiler();
-        JavaKlass javaKlass = javaRuntimeDecompiler.decompileClass(JavaKlass.class);
-        System.out.println(javaKlass.getMajorVersion() + "." + javaKlass.getMinorVersion());
-        System.out.println(javaKlass.getClassName());
-        for (JavaMethod method : javaKlass.getMethods()) {
-            System.out.println(method.getMethodName() + "  " + method.getMethodSignature());
-            for (byte codeByte : method.getCodeBytes()) {
-                System.out.print((codeByte & 0xFF) + " ");
-            }
-            System.out.println();
-            for (JavaInstruction instruction : method.getMethodInstructions()) {
-                System.out.println("  * " + instruction.getOpcode() + " " + instruction.getArguments());
-            }
-        }
-        for (JavaField field : javaKlass.getFields()) {
-            System.out.println(field.getFieldName() + "  " + field.getFieldSignature());
-        }
     }
 }
---
+++ src/test/JrdTest.java
@@ -0,0 +1,29 @@
+package test;
+
+import fr.noctu.jrd.JavaRuntimeDecompiler;
+import fr.noctu.jrd.javaobjects.JavaField;
+import fr.noctu.jrd.javaobjects.JavaKlass;
+import fr.noctu.jrd.javaobjects.JavaMethod;
+import fr.noctu.jrd.javaobjects.utils.JavaInstruction;
+
+public class JrdTest {
+    public static void main(String[] args) {
+        JavaRuntimeDecompiler javaRuntimeDecompiler = new JavaRuntimeDecompiler(); //Instancing a new javaruntime decomp
+        JavaKlass javaKlass = javaRuntimeDecompiler.decompileClass(JavaKlass.class); //decompile the class JavaKlass
+        System.out.println(javaKlass.getMajorVersion() + "." + javaKlass.getMinorVersion()); // printing the version of the class
+        System.out.println(javaKlass.getClassName()); // printing the class name
+        for (JavaMethod method : javaKlass.getMethods()) { // for on the methods
+            System.out.println(method.getMethodName() + "  " + method.getMethodSignature()); // printing the name and signature of the method
+            for (byte codeByte : method.getCodeBytes()) { // for on the method code bytes
+                System.out.print((codeByte & 0xFF) + " "); // print each byte of the method code
+            }
+            System.out.println();
+            for (JavaInstruction instruction : method.getMethodInstructions()) { // for on the method instructions
+                System.out.println("  * " + instruction.getOpcode() + " " + instruction.getArguments()); // print the instruction name and arguments
+            }
+        }
+        for (JavaField field : javaKlass.getFields()) { // for on the fields
+            System.out.println(field.getFieldName() + "  " + field.getFieldSignature()); // print the field name and signature
+        }
+    }
+}
