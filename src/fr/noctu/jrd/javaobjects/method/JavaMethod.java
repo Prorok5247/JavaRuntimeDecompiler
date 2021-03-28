b8ed91aa51a00353d661aed0c4bd93fec689da91
NOcturnais <NOcturnais1412@gmail.com>
2021-03-28 14:10:01 +0200

    fixed things

--- src/fr/noctu/jrd/javaobjects/method/JavaMethod.java
+++ src/fr/noctu/jrd/javaobjects/method/JavaMethod.java
@@ -86,138 +86,142 @@ public class JavaMethod {
                         JavaInstruction javaInstruction = new JavaInstruction(value, codeStartAddress + i);
                         if(value.getArgsNumber() != 0) {
                             bytesToSkip += value.getArgsNumber();
-                            switch (value){
-                                case ASTORE:
-                                case FSTORE:
-                                case ISTORE:
-                                case LSTORE:
-                                case DSTORE:
-                                case ALOAD:
-                                case FLOAD:
-                                case ILOAD:
-                                case LLOAD:
-                                case DLOAD:
-                                case BIPUSH:
-                                    javaInstruction.appendArg(Byte.toString(codeBytes[i + 1]));
-                                    break;
-                                case IINC:
-                                    javaInstruction.appendArg(codeBytes[i + 1] + " " + codeBytes[i + 2]);
-                                    break;
-                                case SIPUSH:
-                                    short intermediateShort = JVMUtils.getShortFromTwoBytes(codeBytes[i + 1], codeBytes[i + 2]);
-                                    javaInstruction.appendArg(String.valueOf(JVMUtils.signExtendIntFromShort(intermediateShort)));
-                                    break;
-                                case IF_ICMPEQ:
-                                case IF_ICMPNE:
-                                case IF_ICMPIT:
-                                case IF_ICMPGE:
-                                case IF_ICMPGT:
-                                case IF_ICMPIE:
-                                case IF_ACMPEQ:
-                                case IF_ACMPNE:
-                                case IFEQ:
-                                case IFGE:
-                                case IFGT:
-                                case IFIE:
-                                case IFIT:
-                                case IFNE:
-                                case IFNONNULL:
-                                case IFNULL:
-                                case GOTO:
-                                    short intermediateShort1 = JVMUtils.getShortFromTwoBytes(codeBytes[i + 1], codeBytes[i + 2]); //this is the label destination
-                                    javaInstruction.appendArg("L" + JVMUtils.signExtendIntFromShort(intermediateShort1));
-                                    break;
-                                case GOTO_W:
-                                    int destInt = JVMUtils.getIntFromFourBytes(codeBytes[i + 1], codeBytes[i + 2], codeBytes[i + 3], codeBytes[i + 4]); //this is the label destination
-                                    javaInstruction.appendArg("L" + destInt);
-                                    break;
-                                case LDC:
-                                    byte indexByte = codeBytes[i + 1];
-                                    int tag = javaConstantPool.tagAt(indexByte);
-                                    switch (tag){
-                                        case 7:
-                                            javaInstruction.appendArg(javaConstantPool.getConstantPoolObject().getClassAt(indexByte).getName().replaceAll("\\.", "/"));
-                                            break;
-                                        case 3:
-                                            javaInstruction.appendArg(String.valueOf(javaConstantPool.getConstantPoolObject().getIntAt(indexByte)));
-                                            break;
-                                        case 4:
-                                            javaInstruction.appendArg(String.valueOf(javaConstantPool.getConstantPoolObject().getFloatAt(indexByte)));
-                                            break;
-                                        case 8:
-                                            javaInstruction.appendArg(javaConstantPool.getConstantPoolObject().getStringAt(indexByte));
-                                            break;
-                                    }
-                                    break;
-                                case LDC_W:
-                                    short cpShortIndex = JVMUtils.getShortFromTwoBytes(codeBytes[i + 1], codeBytes[i + 2]);
-                                    int cpTag = javaConstantPool.tagAt(cpShortIndex);
-                                    switch (cpTag){
-                                        case 7:
-                                            javaInstruction.appendArg(javaConstantPool.getConstantPoolObject().getClassAt(cpShortIndex).getName().replaceAll("\\.", "/"));
-                                            break;
-                                        case 3:
-                                            javaInstruction.appendArg(String.valueOf(javaConstantPool.getConstantPoolObject().getIntAt(cpShortIndex)));
-                                            break;
-                                        case 4:
-                                            javaInstruction.appendArg(String.valueOf(javaConstantPool.getConstantPoolObject().getFloatAt(cpShortIndex)));
-                                            break;
-                                        case 8:
-                                            javaInstruction.appendArg(javaConstantPool.getConstantPoolObject().getStringAt(cpShortIndex));
-                                            break;
-                                    }
-                                    break;
-                                case LDC2_W:
-                                    short cpShortIndex1 = JVMUtils.getShortFromTwoBytes(codeBytes[i + 1], codeBytes[i + 2]);
-                                    int cpTag1 = javaConstantPool.tagAt(cpShortIndex1);
-                                    switch (cpTag1){
-                                        case 5:
-                                            javaInstruction.appendArg(String.valueOf(javaConstantPool.getConstantPoolObject().getLongAt(cpShortIndex1)));
-                                            break;
-                                        case 6:
-                                            javaInstruction.appendArg(String.valueOf(javaConstantPool.getConstantPoolObject().getDoubleAt(cpShortIndex1)));
-                                            break;
-                                    }
-                                    break;
-                                case INSTANCEOF:
-                                case NEW:
-                                case ANEWARRAY:
-                                case CHECKCAST:
-                                    short index = JVMUtils.getShortFromTwoBytes(codeBytes[i + 1], codeBytes[i + 2]);
-                                    javaInstruction.appendArg(javaConstantPool.getConstantPoolObject().getClassAt(index).getName().replaceAll("\\.", "/"));
-                                    break;
-                                case NEWARRAY:
-                                    int arrayType = codeBytes[i + 1];
-                                    switch (arrayType){
-                                        case 4:
-                                            javaInstruction.appendArg("T_BOOLEAN");
-                                            break;
-                                        case 5:
-                                            javaInstruction.appendArg("T_CHAR");
-                                            break;
-                                        case 6:
-                                            javaInstruction.appendArg("T_FLOAT");
-                                            break;
-                                        case 7:
-                                            javaInstruction.appendArg("T_DOUBLE");
-                                            break;
-                                        case 8:
-                                            javaInstruction.appendArg("T_BYTE");
-                                            break;
-                                        case 9:
-                                            javaInstruction.appendArg("T_SHORT");
-                                            break;
-                                        case 10:
-                                            javaInstruction.appendArg("T_INT");
-                                            break;
-                                        case 11:
-                                            javaInstruction.appendArg("T_LONG");
-                                            break;
-                                    }
-                                    break;
-                                case RET:
-                                    javaInstruction.appendArg(String.valueOf(JVMUtils.byteToUnsignedByte(codeBytes[i + 1])));
-                                    break;
+                            try{
+                                switch (value){
+                                    case ASTORE:
+                                    case FSTORE:
+                                    case ISTORE:
+                                    case LSTORE:
+                                    case DSTORE:
+                                    case ALOAD:
+                                    case FLOAD:
+                                    case ILOAD:
+                                    case LLOAD:
+                                    case DLOAD:
+                                    case BIPUSH:
+                                        javaInstruction.appendArg(Byte.toString(codeBytes[i + 1]));
+                                        break;
+                                    case IINC:
+                                        javaInstruction.appendArg(codeBytes[i + 1] + " " + codeBytes[i + 2]);
+                                        break;
+                                    case SIPUSH:
+                                        short intermediateShort = JVMUtils.getShortFromTwoBytes(codeBytes[i + 1], codeBytes[i + 2]);
+                                        javaInstruction.appendArg(String.valueOf(JVMUtils.signExtendIntFromShort(intermediateShort)));
+                                        break;
+                                    case IF_ICMPEQ:
+                                    case IF_ICMPNE:
+                                    case IF_ICMPIT:
+                                    case IF_ICMPGE:
+                                    case IF_ICMPGT:
+                                    case IF_ICMPIE:
+                                    case IF_ACMPEQ:
+                                    case IF_ACMPNE:
+                                    case IFEQ:
+                                    case IFGE:
+                                    case IFGT:
+                                    case IFIE:
+                                    case IFIT:
+                                    case IFNE:
+                                    case IFNONNULL:
+                                    case IFNULL:
+                                    case GOTO:
+                                        short intermediateShort1 = JVMUtils.getShortFromTwoBytes(codeBytes[i + 1], codeBytes[i + 2]); //this is the label destination
+                                        javaInstruction.appendArg("L" + JVMUtils.signExtendIntFromShort(intermediateShort1));
+                                        break;
+                                    case GOTO_W:
+                                        int destInt = JVMUtils.getIntFromFourBytes(codeBytes[i + 1], codeBytes[i + 2], codeBytes[i + 3], codeBytes[i + 4]); //this is the label destination
+                                        javaInstruction.appendArg("L" + destInt);
+                                        break;
+                                    case LDC:
+                                        byte indexByte = codeBytes[i + 1];
+                                        int tag = javaConstantPool.tagAt(indexByte);
+                                        switch (tag){
+                                            case 7:
+                                                javaInstruction.appendArg(javaConstantPool.getConstantPoolObject().getClassAt(indexByte).getName().replaceAll("\\.", "/"));
+                                                break;
+                                            case 3:
+                                                javaInstruction.appendArg(String.valueOf(javaConstantPool.getConstantPoolObject().getIntAt(indexByte)));
+                                                break;
+                                            case 4:
+                                                javaInstruction.appendArg(String.valueOf(javaConstantPool.getConstantPoolObject().getFloatAt(indexByte)));
+                                                break;
+                                            case 8:
+                                                javaInstruction.appendArg(javaConstantPool.getConstantPoolObject().getStringAt(indexByte));
+                                                break;
+                                        }
+                                        break;
+                                    case LDC_W:
+                                        short cpShortIndex = JVMUtils.getShortFromTwoBytes(codeBytes[i + 1], codeBytes[i + 2]);
+                                        int cpTag = javaConstantPool.tagAt(cpShortIndex);
+                                        switch (cpTag){
+                                            case 7:
+                                                javaInstruction.appendArg(javaConstantPool.getConstantPoolObject().getClassAt(cpShortIndex).getName().replaceAll("\\.", "/"));
+                                                break;
+                                            case 3:
+                                                javaInstruction.appendArg(String.valueOf(javaConstantPool.getConstantPoolObject().getIntAt(cpShortIndex)));
+                                                break;
+                                            case 4:
+                                                javaInstruction.appendArg(String.valueOf(javaConstantPool.getConstantPoolObject().getFloatAt(cpShortIndex)));
+                                                break;
+                                            case 8:
+                                                javaInstruction.appendArg(javaConstantPool.getConstantPoolObject().getStringAt(cpShortIndex));
+                                                break;
+                                        }
+                                        break;
+                                    case LDC2_W:
+                                        short cpShortIndex1 = JVMUtils.getShortFromTwoBytes(codeBytes[i + 1], codeBytes[i + 2]);
+                                        int cpTag1 = javaConstantPool.tagAt(cpShortIndex1);
+                                        switch (cpTag1){
+                                            case 5:
+                                                javaInstruction.appendArg(String.valueOf(javaConstantPool.getConstantPoolObject().getLongAt(cpShortIndex1)));
+                                                break;
+                                            case 6:
+                                                javaInstruction.appendArg(String.valueOf(javaConstantPool.getConstantPoolObject().getDoubleAt(cpShortIndex1)));
+                                                break;
+                                        }
+                                        break;
+                                    case INSTANCEOF:
+                                    case NEW:
+                                    case ANEWARRAY:
+                                    case CHECKCAST:
+                                        short index = JVMUtils.getShortFromTwoBytes(codeBytes[i + 1], codeBytes[i + 2]);
+                                        javaInstruction.appendArg(javaConstantPool.getConstantPoolObject().getClassAt(index).getName().replaceAll("\\.", "/"));
+                                        break;
+                                    case NEWARRAY:
+                                        int arrayType = codeBytes[i + 1];
+                                        switch (arrayType){
+                                            case 4:
+                                                javaInstruction.appendArg("T_BOOLEAN");
+                                                break;
+                                            case 5:
+                                                javaInstruction.appendArg("T_CHAR");
+                                                break;
+                                            case 6:
+                                                javaInstruction.appendArg("T_FLOAT");
+                                                break;
+                                            case 7:
+                                                javaInstruction.appendArg("T_DOUBLE");
+                                                break;
+                                            case 8:
+                                                javaInstruction.appendArg("T_BYTE");
+                                                break;
+                                            case 9:
+                                                javaInstruction.appendArg("T_SHORT");
+                                                break;
+                                            case 10:
+                                                javaInstruction.appendArg("T_INT");
+                                                break;
+                                            case 11:
+                                                javaInstruction.appendArg("T_LONG");
+                                                break;
+                                        }
+                                        break;
+                                    case RET:
+                                        javaInstruction.appendArg(String.valueOf(JVMUtils.byteToUnsignedByte(codeBytes[i + 1])));
+                                        break;
+                                }
+                            } catch (Exception e){
+                                System.err.println("Arguments parsing crashed on instruction: " + value + " at index: " + i);
                             }
                         }
                         javaInstructions.add(javaInstruction);
